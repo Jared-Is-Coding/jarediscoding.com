f268c29eddc1c98f3b6cf2107eaf2d64b3dc46ba
Jared Harrison <jared@jarediscoding.com>
2023-04-24 17:25:03 -0400

    Placeholder homepage, Add metadata component
    - Homepage now using placeholder information
    - Metadata component is live
    - Add sass plugin
    - Add base styles
    - Create static dir

---
+++ src/components/siteMetaData.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { useSiteMetaData } from "../hooks/useSiteMetaData"
+
+type searchEngineOptimizationProps = {
+    title?: string
+    description?: string
+    image?: string
+}
+
+export const siteMetaData = ({ title, description, image }: searchEngineOptimizationProps) => {
+    const { title: defaultTitle, description: defaultDescription, image: defaultImage, siteUrl } = useSiteMetaData()
+
+    return (
+        <>
+            {/* Site name */}
+            <title>{title || defaultTitle}</title>
+            <meta name="site_name" content={title || defaultTitle} />
+            <meta property="og:title" content={title || defaultTitle} />
+
+            {/* Site icon */}
+            <meta property="og:image" content={image || siteUrl + defaultImage}></meta>
+            <link rel="icon" href={image || siteUrl + defaultImage} />
+
+            {/* Site description */}
+            <meta name="description" content={description || defaultDescription} />
+            
+            {/* Site properties */}
+            <meta property="og:type" content="website" />
+            <meta property="og:locale" content="en_US" />
+
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,88 +1,49 @@
+import { Link, type HeadFC, type PageProps } from "gatsby"
 import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
-
-/** 
- * Website name to be used across the browser and embeds
- */
-const pageTitle = "Jared Is Coding"
+import { siteMetaData } from "../components/siteMetaData"
+import "../css/styles.scss"
 
 export const Head: HeadFC = () => (
-    <>
-        {/* Site name */}
-        <title>{pageTitle}</title>
-        <meta name="site_name" content={pageTitle} />
-        <meta property="og:title" content={pageTitle} />
-        
-        {/* Site properties */}
-        <meta property="og:type" content="website" />
-
-        {/* Site is responsive */}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </>
+    siteMetaData({})
 )
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
+const projectData = [
+    {
+        text: "Elemental Bot for Discord",
+        url: "https://elementalbot.com/",
+        target: "_blank"
+    },
+    {
+        text: "JaredIsCoding.com",
+        url: "https://JaredIsCoding.com/",
+        target: "_self"
+    }
 ]
 
 const IndexPage: React.FC<PageProps> = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Welcome
-      </h1>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </main>
-  )
+    return (
+        <>
+            <main className="flex-col flex-wrap flex-center">
+                <div className="col-item">
+                    <h1>
+                        Work in Progress
+                    </h1>
+                </div>
+                <div className="col-item">
+                    <p>In the mean time, here are some of Jared's projects</p>
+                </div>
+                <div className="col-item flex-row">
+                    {projectData.map((project) => (
+                        <div className="row-item card">
+                            <Link to={project.url} target={project.target}>
+                                {project.text}
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </main>
+        </>
+    )
 }
 
 export default IndexPage
