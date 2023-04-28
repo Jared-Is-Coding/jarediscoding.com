409e76e267a9608039311a40b6ba9223fe885f79
Jared H <jared@jarediscoding.com>
2023-04-28 13:37:29 -0400

    Indents, Link title

--- src/components/StickyFooter.tsx
+++ src/components/StickyFooter.tsx
@@ -5,7 +5,7 @@ import { FaGithub } from "react-icons/fa"
 export const StickyFooter = () => (
     <footer className="flex-row flex-center">
         <Link to="https://github.com/Jared-Is-Coding/jarediscoding.com" target="_blank">
-            <FaGithub size="2em" />
+            <FaGithub size="2em" title="GitHub icon" />
         </Link>
     </footer>
 )
