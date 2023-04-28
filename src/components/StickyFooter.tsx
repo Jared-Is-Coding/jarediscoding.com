0608798305c2520dc55ced7ae0a50cd7bbfb510d
Jared H <jared@jarediscoding.com>
2023-04-28 13:46:07 -0400

    Link accessibility

--- src/components/StickyFooter.tsx
+++ src/components/StickyFooter.tsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa"
 
 export const StickyFooter = () => (
     <footer className="flex-row flex-center">
-        <Link to="https://github.com/Jared-Is-Coding/jarediscoding.com" target="_blank">
+        <Link to="https://github.com/Jared-Is-Coding/jarediscoding.com" target="_blank" title="Link to GitHub.com">
             <FaGithub size="2em" title="GitHub icon" />
         </Link>
     </footer>
