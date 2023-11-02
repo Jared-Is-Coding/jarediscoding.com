336a519a8b5940b8b05a38fc08b662b9331f8ae5
Jared H <jared@jarediscoding.com>
2023-11-01 22:55:09 -0400

    NPM update, fix gatsby-ndoe

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,24 +1,20 @@
-import type { GatsbyNode } from "gatsby"
+import type { CreatePagesArgs } from "gatsby"
 
-const gatsbyNode: GatsbyNode = {
-    createPages: async ({ graphql, actions }) => {
-        const { createRedirect } = actions
-    
-        const redirects = [
-            { from: "/t", to: "/tools", permanent: true },
-            { from: "/t/*", to: "/tools", permanent: false }
-        ]
-      
-        for (const thisRedirect of redirects) {
-            createRedirect({
-                fromPath: thisRedirect.from,
-                toPath: thisRedirect.to,
-                isPermanent: thisRedirect.permanent,
-                force: true,
-                redirectInBrowser: true
-            })
-        }
+exports.createPages = ({ actions }: CreatePagesArgs) => {
+    const { createRedirect } = actions
+
+    const redirects = [
+        { from: "/t", to: "/tools", permanent: true },
+        { from: "/t/*", to: "/tools", permanent: false }
+    ]
+
+    for (const thisRedirect of redirects) {
+        createRedirect({
+            fromPath: thisRedirect.from,
+            toPath: thisRedirect.to,
+            isPermanent: thisRedirect.permanent,
+            force: true,
+            redirectInBrowser: true
+        })
     }
 }
-
-export default gatsbyNode
