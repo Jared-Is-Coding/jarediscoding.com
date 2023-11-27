b575195e5cdb1ce2f165a3e46fcc4098fa1f6ef0
Jared Harrison <jared@jarediscoding.com>
2023-11-27 10:16:39 -0500

    NPM update, local bootstrap

--- src/components/MetaData.tsx
+++ src/components/MetaData.tsx
@@ -33,8 +33,20 @@ export const MetaData = ({ title, description, image }: searchEngineOptimization
             <link rel="manifest" href="/manifest.webmanifest"></link>
 
             {/* Bootstrap */}
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossOrigin="anonymous"></link>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous"></script>
+            {(siteUrl.includes("localhost"))
+                ? (
+                    <>
+                        <link href="/css/bootstrap.local.css" rel="stylesheet"></link>
+                        <script src="/js/bootstrap.local.js"></script>
+                    </>
+                )
+                : (
+                    <>
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossOrigin="anonymous"></link>
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous"></script>
+                    </>
+                )
+            }
         </>
     )
 }
