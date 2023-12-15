883bc105b32d3f2d5ffad898bcb6ef336571a657
Jared H <jared@jarediscoding.com>
2023-12-15 11:02:27 -0500

    Adjust font sizes

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -4,10 +4,10 @@ import { FaEnvelope, FaGithub } from "react-icons/fa"
 export const SiteFooter = () => (
     <footer className="flex-row flex-center">
         <a href="mailto:jared@jarediscoding.com?subject=Inquiry from Website JaredIsCoding.com" target="_blank" title="Link to send me an email">
-            <FaEnvelope size="2em" title="Send me an email" />
+            <FaEnvelope size="1.5em" title="Send me an email" />
         </a>
         <a href="https://github.com/Jared-Is-Coding/jarediscoding.com" target="_blank" title="Link to GitHub.com">
-            <FaGithub size="2em" title="Find me on GitHub.com" />
+            <FaGithub size="1.5em" title="Find me on GitHub.com" />
         </a>
     </footer>
 )
