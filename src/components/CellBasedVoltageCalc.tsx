44350e67f763c8ed405f86630b5c7a0314a97223
Jared H <jared@jarediscoding.com>
2024-05-04 20:07:24 -0400

    Quick voltages now scale based on cell count configuration

--- src/components/CellBasedVoltageCalc.tsx
+++ src/components/CellBasedVoltageCalc.tsx
@@ -2,32 +2,20 @@ import React, { ChangeEvent, Component } from "react"
 import { Alert } from "react-bootstrap"
 import FormRange from "react-bootstrap/FormRange"
 
+const baseCellCount = 15
+const quickVoltageButtonCount = 12
+
 export class CellBasedVoltageCalc extends Component {
     state = {
         voltage: 56.35,
-        cellCount: 15,
+        cellCount: baseCellCount,
         cellVoltage: 3.757,
+        voltageMin: baseCellCount * 2.85,
+        voltageMax: baseCellCount * 4.20,
         result: "49.84%",
         showSaved: false,
         showCleared: false
     }
-
-    quickVoltages = [
-        50.00,
-        55.00,
-        60.00,
-        65.00,
-        70.00,
-        75.00,
-        80.00,
-        85.00,
-        90.00,
-        95.00,
-        100.00,
-        105.00,
-        110.00,
-        115.00
-    ]
 
     cellCounts = [
         [15, "(XR, Pint, Pint-X)"],
@@ -54,7 +42,10 @@ export class CellBasedVoltageCalc extends Component {
         
         if (this.state.voltage == value) return
         
-        this.setState({ voltage: value.toFixed(2), cellVoltage: (value / this.state.cellCount).toFixed(3) }, this.doCalculation)
+        this.setState({
+            voltage: value.toFixed(2),
+            cellVoltage: (value / this.state.cellCount).toFixed(3)
+        }, this.doCalculation)
     }
 
     setCellCount = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -66,7 +57,12 @@ export class CellBasedVoltageCalc extends Component {
         
         if (this.state.cellCount == value) return
         
-        this.setState({ cellCount: value.toFixed(0), cellVoltage: (this.state.voltage / value).toFixed(3) }, this.doCalculation)
+        this.setState({
+            cellCount: value.toFixed(0),
+            cellVoltage: (this.state.voltage / value).toFixed(3),
+            voltageMin: value * 2.85,
+            voltageMax: value * 4.20
+        }, this.doCalculation)
     }
 
     doCalculation = () => {
@@ -87,7 +83,9 @@ export class CellBasedVoltageCalc extends Component {
             - 2.8
         )
 
-        this.setState({ result: `${calcPercentage.toFixed(2)}%` })
+        this.setState({
+            result: `${calcPercentage.toFixed(2)}%`
+        })
     }
 
     componentDidMount() {
@@ -97,8 +95,10 @@ export class CellBasedVoltageCalc extends Component {
             if (savedVoltage && savedCellCount) {
                 this.setState({
                     voltage: parseFloat(savedVoltage).toFixed(2),
-                    cellVoltage: (parseFloat(savedVoltage) / parseFloat(savedCellCount)).toFixed(3),
-                    cellCount: parseFloat(savedCellCount).toFixed(0)
+                    cellVoltage: (parseFloat(savedVoltage) / parseInt(savedCellCount)).toFixed(3),
+                    cellCount: parseInt(savedCellCount),
+                    voltageMin: parseInt(savedCellCount) * 2.85,
+                    voltageMax: parseInt(savedCellCount) * 4.20
                 }, this.doCalculation)
             }
     }
@@ -142,9 +142,24 @@ export class CellBasedVoltageCalc extends Component {
 
                 <label>Quick Voltage</label>
                 <div className="flex-row flex-center full-width">
-                    {this.quickVoltages.map((v, i) => (
-                        <button key={`voltageButton-${i}`} className="padded col-sm-3" onClick={e => this.setVoltage(v)}>{v.toFixed(1)}V</button>
-                    ))}
+                    {
+                        [...Array(quickVoltageButtonCount)].map((v, i) => {
+                            const buttonVoltage = Math.round(
+                                this.state.voltageMin
+                                + (
+                                    (this.state.voltageMax - this.state.voltageMin)
+                                    / quickVoltageButtonCount
+                                    * (i + 1)
+                                )
+                            )
+
+                            return (
+                                <button key={`voltageButton-${i}`} className="padded col-sm-3" onClick={e => this.setVoltage(buttonVoltage)}>
+                                    {buttonVoltage.toFixed(2)}V
+                                </button>
+                            )
+                        })
+                    }
                 </div>
 
                 <br />
